bb392dc4aa16db7d05acf6b210bbb6deb83216ef
Apti <aptia@thinkeasy.cz>
2024-10-11 13:14:22 +0200

    init project, add some views, set up storage

---
+++ photoapp_fe/src/components/layout/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>Photo CRM</strong>
+            </router-link>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <router-link to="/my-account" class="button is-succes my-account-btn"><strong>My Account</strong></router-link>
+                        <router-link to="/sign-up" class="button is-succes"><strong>Sign up</strong></router-link>
+                        <router-link to="/log-in" class="button is-light">Log in</router-link>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style scoped>
+.my-account-btn {
+    background-color: #4CAF50; /* Например, другой зеленый цвет */
+    color: white;
+    font-weight: bold;
+}
+</style>
---
+++ photoapp_fe/src/router/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
+
+
+import HomeView from '../views/HomeView.vue'
+
+import SignUp from '@/views/SignUp.vue'
+import LogIn from '@/views/LogIn.vue'
+import MyAccount from '@/views/dashboard/MyAccount.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
+})
+
+export default router

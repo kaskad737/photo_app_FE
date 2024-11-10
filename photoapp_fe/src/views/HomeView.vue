1ce9ce663b8792cce4f8b13ef9795843bdd77c50
Apti <aptia@thinkeasy.cz>
2024-11-10 15:59:43 +0100

    modify home view

--- photoapp_fe/src/views/HomeView.vue
+++ photoapp_fe/src/views/HomeView.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="home">
-    <router-link to="/image-download" class="button is-primary is-large is-fullwidth">
-      Download Image
+  <div class="button-container">
+    <router-link to="/image-upload" class="button is-primary is-large">
+      Upload Image
+    </router-link>
+    <router-link to="/frame-upload" class="button is-primary is-large">
+      Upload Frame
     </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView'
   }
-}
 </script>
+
+<style scoped>
+  .button-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center; 
+  }
+
+  .button {
+    width: 150px; 
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white; 
+    font-size: 16px; 
+    text-decoration: none; 
+    border-radius: 8px; 
+  }
+
+  .button:hover {
+    background-color: #3273dc; 
+  }
+</style>

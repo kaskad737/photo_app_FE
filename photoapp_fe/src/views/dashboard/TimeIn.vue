f0df906d8093acd65aecaea35b60f8f7261bfb37
Maria Vasylchenko <134641398+mariavsl518@users.noreply.github.com>
2024-11-12 15:18:41 +0100

    Update TimeIn.vue

--- photoapp_fe/src/views/dashboard/TimeIn.vue
+++ photoapp_fe/src/views/dashboard/TimeIn.vue
@@ -2,11 +2,11 @@
     <router-link to="/my-account" class="button">
         Back
     </router-link>
-    <form action="" class="field">
+    <form @submit.prevent="submitForm" class="field">
         <label for="restaurants">
             Select your restaurant
         </label>
-        <select name="" id="restaurants" class="select">
+        <select name="" v-model="restaurants" class="select">
             <option value="restaurant1">Restaurant 1</option>
             <option value="restaurant2">Restaurant 2</option>
             <option value="restaurant3">Restaurant 3</option>
@@ -15,32 +15,32 @@
         <label for="time">
             Time IN
         </label>
-        <input class='input' type="time" id="time" required>
+        <input class='input' type="time" v-model="time" required>
 
         <label for="name">
             Name
         </label>
-        <input class='input' type="text" id="name" required />
+        <input class='input' type="text" v-model="name" required />
 
         <label for="frames">
             Number of frames
         </label>
-        <input class='input' type="number" id="frames" required>
+        <input class='input' type="number" v-model="frames" required>
 
         <label for="sets">
             Number of sets
         </label>
-        <input class='input' type="number" id="sets" required>
+        <input class='input' type="number" v-model="sets" required>
 
         <label for="printer">
             Printer life
         </label>
-        <input class='input' type="text" id="printer" required>
+        <input class='input' type="text" v-model="printer" required>
 
         <label for="cash">
             Cash
         </label>
-        <input class='input' type="number" required>
+        <input class='input' type="number" v-model="cash" required>
 
         <button class="button" type="submit">
             Submit
@@ -51,12 +51,31 @@
 <script>
 
 export default {
-    name: 'TimeIn'
-}
-
-function submitForm(params) {
-    formData = {
-        
+    name: 'TimeIn',
+    data() {
+        return {
+            restaurant: '',
+            time: '',
+            name: '',
+            frames: '',
+            sets: '',
+            printer: '',
+            cash:''
+        };
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                restaurant: this.restaurants,
+                time: this.time,
+                name: this.name,
+                frames: this.frames,
+                sets: this.sets,
+                printer: this.printer,
+                cash: this.cash,
+            };
+            console.log(formData)
+        }
     }
 }
 

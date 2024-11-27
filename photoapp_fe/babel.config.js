861570fe265a786d68b76d17d629e0d262db3c93
Apti <aptia@thinkeasy.cz>
2024-11-27 21:10:38 +0100

    update configs and packages

--- photoapp_fe/babel.config.js
+++ photoapp_fe/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ['@vue/cli-plugin-babel/preset'],
+  overrides: [
+    {
+      test: /node_modules\/oh-vue-icons\/icons/,
+      compact: true, // Skip deoptimization for large files
+    },
+  ],
+};

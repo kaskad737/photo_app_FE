c43a1fcaa43763d894e8a34aa6f4c91e62171023
Apti <aptia@thinkeasy.cz>
2024-11-30 15:39:35 +0100

    finish my account view

--- photoapp_fe/src/views/dashboard/MyAccount.vue
+++ photoapp_fe/src/views/dashboard/MyAccount.vue
@@ -3,34 +3,63 @@
 
         <router-link to="/"
             class="button"><strong>← Back</strong>
-        </router-link>
+        </router-link>   
 
         <div class="column is-4 is-offset-4">
-            <h1 class="title">My account</h1>
-            <p> Username: {{ username }}</p>
+            <h1 class="title">User Details</h1>
+            <div v-if="user">
+                <p class="user-detail"><strong>Full Name:</strong> {{ user.first_name }} {{ user.last_name }}</p>
+                <p class="user-detail"><strong>Role:</strong> {{ user.role }}</p>
+                <p class="user-detail"><strong>Email:</strong> {{ user.email }}</p>
+            </div>
+            <div v-else>
+                <p>Loading...</p>
+            </div>
         </div>
     </div>
-</template>
-
+  </template>
 <script>
-    export default {
-        name: 'MyAccount',
-        data() {
-            return {
-                username: '',
+  import axios from "axios";
+  
+  export default {
+    name: "MyAccount",
+    data() {
+      return {
+        user: null,
+      };
+    },
+    methods: {
+        async fetchUser() {
+        const userId = localStorage.getItem('userId');
+        if (!userId) {
+            console.error("User ID not found in localStorage");
+            return;
             }
-            },
-        mounted() {
-            this.getUsername();
-            },
-        methods: {
-            getUsername() {
-                this.username = localStorage.getItem('userFirstName') || localStorage.getItem('username');
-                },
-            },
-        }
-</script>
 
+        try {
+            const response = await axios.get(`auth/users/${userId}`);
+            this.user = response.data;
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+            }
+        },
+    },
+    created() {
+      this.fetchUser();
+    },
+  };
+</script>
+  
 <style scoped>
+
+
+.user-detail {
+  margin-bottom: 10px;
+}
+
+.user-detail strong {
+  color: #ffffff;
+  font-weight: bold;
+}
 
 </style>

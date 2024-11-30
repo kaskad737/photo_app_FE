b9afd40f0bc51db552e1b394f54650cf4e8d03f2
Apti <aptia@thinkeasy.cz>
2024-11-30 22:32:27 +0100

    Make first letter of role capitalized

--- photoapp_fe/src/views/dashboard/MyAccount.vue
+++ photoapp_fe/src/views/dashboard/MyAccount.vue
@@ -9,7 +9,7 @@
             <h1 class="title">User Details</h1>
             <div v-if="user">
                 <p class="user-detail"><strong>Full Name:</strong> {{ user.first_name }} {{ user.last_name }}</p>
-                <p class="user-detail"><strong>Role:</strong> {{ user.role }}</p>
+                <p class="user-detail"><strong>Role:</strong> {{ userRole }}</p>
                 <p class="user-detail"><strong>Email:</strong> {{ user.email }}</p>
             </div>
             <div v-else>
@@ -26,6 +26,7 @@
     data() {
       return {
         user: null,
+        userRole: '',
       };
     },
     methods: {
@@ -39,6 +40,7 @@
         try {
             const response = await axios.get(`auth/users/${userId}`);
             this.user = response.data;
+            this.userRole = this.user.role.charAt(0).toUpperCase() + this.user.role.slice(1);
         } catch (error) {
             console.error("Error fetching user data:", error);
             }

0bd7e6af859cec10b753bcae965d6106dce3a01c
Apti <aptia@thinkeasy.cz>
2024-12-01 17:18:56 +0100

    optimize code to work faster

--- photoapp_fe/src/views/ImageDownload.vue
+++ photoapp_fe/src/views/ImageDownload.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="container">
+    <router-link v-if="isAuthenticated" to="/list-images" class="button"><strong>← Back</strong></router-link>
 
-    <router-link v-if="isAuthenticated" to="/list-images"
-      class="button"><strong>← Back</strong>
-    </router-link>
-
-    <div class="image-container">
+    <div v-for="(image, index) in images" :key="index" class="image-container">
       <h1 class="title">Download your photo</h1>
-      <h2 class="title is-4">Image: </h2>
-      <img :src="imageUrl" alt="Downloadable" class="center-image" />
-      <button class="download-button" @click="downloadImage">Download</button>
-    </div>
-    <div class="image-container">
-      <h2 class="title is-4">Image in frame: </h2>
-      <img :src="imageInFrameUrl" alt="Downloadable" class="center-image" />
-      <button class="download-button" @click="downloadImageInFrame">Download</button>
-    </div>
-    <div class="image-container">
-      <h2 class="title is-4">Image in frame with QR-Code: </h2>
-      <img :src="imageInFrameWithQrCodeUrl" alt="Downloadable" class="center-image" />
-      <button class="download-button" @click="downloadImageInWithQrCodeFrame">Download</button>
+      <h2 class="title is-4">Image{{ index + 1 }}: </h2>
+      <img :src="image.url" alt="Downloadable" class="center-image" />
+      <button class="download-button" @click="downloadImage(image.url, image.name)">Download</button>
     </div>
   </div>
 </template>
-  
+
 <script>
   import { mapState } from 'vuex';
   import axios from 'axios';
@@ -31,104 +18,54 @@
   export default {
     name: "ImageDownload",
     computed: {
-        ...mapState(['isAuthenticated']),
+      ...mapState(['isAuthenticated']),
     },
     data() {
       return {
         imageId: '',
-        imageUrl: '', 
-        imageInFrameUrl: '', 
-        imageName: '', 
-        imageInFrameName: '', 
-        imageInFrameWithQrCodeUrl: '',
-        imageInFrameWithQrCodeName: '',
-
+        images: [],
       };
     },
-    created() {
-      this.getImageId()
-      this.fetchImage()
-      
+    async created() {
+      await this.getImageId();
+      await this.fetchImage();
     },
     methods: {
+      async getImageId() {
+        this.imageId = this.$route.params.id;
+      },
       async fetchImage() {
         try {
           const response = await axios.get(`image/photo/${this.imageId}`);
-          this.imageUrl = response.data.photo;
-          this.imageName = response.data.photo.name;
-          this.imageInFrameUrl = response.data.photo_in_frame;
-          this.imageInFrameName = response.data.photo_in_frame.name;
-          this.imageInFrameWithQrCodeUrl = response.data.photo_in_frame_with_qr_code;
-          this.imageInFrameWithQrCodeName = response.data.photo_in_frame_with_qr_code.name;
+          const photo = response.data.photo;
+          const photoInFrame = response.data.photo_in_frame;
+          const photoInFrameWithQrCode = response.data.photo_in_frame_with_qr_code;
+
+          // Преобразуем данные для упрощенной работы
+          this.images = [
+            { url: photo, name: photo.name },
+            { url: photoInFrame, name: photoInFrame.name },
+            { url: photoInFrameWithQrCode, name: photoInFrameWithQrCode.name }
+          ];
         } catch (error) {
           console.error("Error fetching images:", error);
         }
       },
-      async getImageId() {
-        this.imageId = this.$route.params.id;
-      },
-      async downloadImage() {
+      async downloadImage(url, name) {
         try {
-          const response = await fetch(this.imageUrl);
-          
+          const response = await fetch(url);
+
           if (!response.ok) {
             throw new Error('Failed to download image');
           }
 
           const blob = await response.blob();
-
-          const url = window.URL.createObjectURL(blob);
-
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = this.imageName;
-          a.click();
-
-          window.URL.revokeObjectURL(url);
-        } catch (error) {
-          console.error('Error downloading image:', error);
-        }
-      },
-      async downloadImageInFrame() {
-        try {
-          const response = await fetch(this.imageInFrameUrl);
-          
-          if (!response.ok) {
-            throw new Error('Failed to download image');
-          }
-
-          const blob = await response.blob();
-
-          const url = window.URL.createObjectURL(blob);
-
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = this.imageInFrameName;
-          a.click();
-
-          window.URL.revokeObjectURL(url);
-        } catch (error) {
-          console.error('Error downloading image:', error);
-        }
-      },
-      async downloadImageInWithQrCodeFrame() {
-        try {
-          const response = await fetch(this.imageInFrameWithQrCodeUrl);
-          
-          if (!response.ok) {
-            throw new Error('Failed to download image');
-          }
-
-          const blob = await response.blob();
-
-          const url = window.URL.createObjectURL(blob);
-
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = this.imageInFrameWithQrCodeName;
-          a.click();
-
-          window.URL.revokeObjectURL(url);
+          const objectUrl = window.URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.href = objectUrl;
+          link.download = name;
+          link.click();
+          window.URL.revokeObjectURL(objectUrl);
         } catch (error) {
           console.error('Error downloading image:', error);
         }
@@ -136,7 +73,7 @@
     },
   };
 </script>
-  
+
 <style scoped>
   .image-container {
     display: flex;
@@ -146,7 +83,7 @@
     min-height: 100vh;
     text-align: center;
   }
-  
+
   .center-image {
     max-width: 90%;
     height: auto;
@@ -154,7 +91,7 @@
     border: 1px solid #ccc;
     border-radius: 10px;
   }
-  
+
   .download-button {
     display: inline-block;
     padding: 10px 20px;
@@ -165,9 +102,8 @@
     border-radius: 5px;
     transition: background-color 0.3s;
   }
-  
+
   .download-button:hover {
     background-color: #0056b3;
   }
 </style>
-  
